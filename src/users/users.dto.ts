cc525654b5755b273da804412fe179679558555b
Byeongmin Lee <bmlee531@gmail.com>
2024-04-26 10:57:33 +0900

    update: userDTO

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,4 +1,17 @@
+import { Timestamp } from 'firebase-admin/firestore';
+
 export interface UserDTO {
+  userId: string;
+  uid: string;
+  email: string;
+  role: 'admin' | 'user';
+  social: 'kakao' | 'google';
+  refreshToken?: string;
+  createdAt: Date | Timestamp;
+  updatedAt: Date | Timestamp;
+}
+
+export interface AuthDTO {
   userId: string;
   email: string;
 }
